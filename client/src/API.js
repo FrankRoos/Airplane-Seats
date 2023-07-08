8c0f1e83d847b0f1dccc2d3bf3e287c53ef26963
Franklin Roosevelt Fofou Ngangnimaze s308705 <s308705@studenti.polito.it>
2023-07-08 19:00:54 +0200

    modify API.js, App.jsx and index.css

--- client/src/API.js
+++ client/src/API.js
@@ -1,20 +1,204 @@
-const APIURL = 'http://localhost:3000';
+const APIURL = 'http://localhost:3000/api';
 
-async function getGuess() {
-
+async function listPlanes() {
     try {
-        const response = await fetch(APIURL + '/guess');
-        if (response.ok)
-        {
-            const n = await response.text() ;
-            return Number(n) ;
-        } else 
-            throw new Error() ;
-    } catch(e) {
-        throw new Error(e) ;
+        const response = await fetch(APIURL + '/planes', {
+            credentials: 'include'
+        });
+        if (response.ok) {
+            const planes = await response.json();
+            return planes;
+        } else {
+            // if response is not OK
+            const message = await response.text();
+            throw new Error(response.statusText + " " + message);
+        }
+    } catch (error) {
+        throw new Error(error.message, { cause: error })
     }
-
-
 }
 
-export { getGuess };
+async function addReservation(date, planeId, planeType) {
+    try {
+        const response = await fetch(APIURL + `/planes/${planeId}/reservation`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': "application/json"
+            },
+            credentials: 'include',
+            body: JSON.stringify({
+                "date": date,
+                "planeType": planeType,
+            })
+        });
+        if (response.ok) {
+            const id = Number(await response.text());
+            return id;
+        } else {
+            const message = await response.text();
+            throw new Error(response.statusText + " " + message);
+        }
+    } catch (error) {
+        throw new Error(error.message, { cause: error });
+    }
+}
+
+async function listReservations() {
+    try {
+        const response = await fetch(APIURL + `/reservations`, {
+            credentials: 'include'
+        });
+        if (response.ok) {
+            const reservations = await response.json();
+            return reservations;
+        } else {
+            // if response is not OK
+            const message = await response.text();
+            throw new Error(response.statusText + " " + message);
+        }
+    } catch (error) {
+        throw new Error(error.message, { cause: error });
+    }
+}
+
+async function deleteReservation(reservationId) {
+    try {
+        const response = await fetch(APIURL + `/reservations/${reservationId}`, {
+            method: 'DELETE',
+            credentials: 'include'
+        });
+
+        if (response.ok) {
+            return true;
+        } else {
+            const message = await response.text();
+            throw new Error(response.statusText + " " + message);
+        }
+    } catch (error) {
+        throw new Error(error.message, { cause: error })
+    }
+}
+
+async function listReservedSeats(reservationId) {
+    try {
+        const response = await fetch(APIURL + `/reservations/${reservationId}/reservedseats`, {
+            credentials: 'include'
+        });
+        if (response.ok) {
+            const reservedSeats = await response.json();
+            return reservedSeats;
+        } else {
+            // if response is not OK
+            const message = await response.text();
+            throw new Error(response.statusText + " " + message);
+        }
+    } catch (error) {
+        throw new Error(error.message, { cause: error });
+    }
+}
+
+async function listSeats(planeId) {
+    try {
+        const response = await fetch(APIURL + `/planes/${planeId}/seats`, {
+            credentials: 'include'
+        });
+        if (response.ok) {
+            const seats = await response.json();
+            return seats;
+        } else {
+            // if response is not OK
+            const message = await response.text();
+            throw new Error(response.statusText + " " + message);
+        }
+    } catch (error) {
+        throw new Error(error.message, { cause: error })
+    }
+}
+
+async function updateSeat(seatId, planeId, code, isReserved, reservationId) {
+    try {
+        const response = await fetch(APIURL + `/planes/${planeId}/seats/${seatId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': "application/json"
+            },
+            credentials: 'include',
+            body: JSON.stringify({
+                "code": code,
+                "isReserved": isReserved,
+                "reservationId": reservationId
+            })
+        });
+        if (response.ok) {
+            return true;
+        } else {
+            const message = response.text();
+            throw new Error(response.statusText + " " + message);
+        }
+    } catch (error) {
+        throw new Error(error.message, { cause: error });
+    }
+}
+
+// LOGIN-LOGOUT APIs
+
+async function doLogin(email, password) {
+    try {
+        const response = await fetch(APIURL + '/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify({
+                "username": email,
+                "password": password
+            })
+        });
+        if (response.ok) {
+            return await response.json();
+        } else {
+            const message = await response.text();
+            throw new Error(response.statusText + " " + message);
+        }
+    } catch (error) {
+        throw new Error(error.message, { cause: error });
+    }
+}
+
+async function getUserInfo(){
+    try {
+        const response = await fetch(APIURL + '/login/current', {
+            credentials: 'include'
+        });
+        if (response.ok) {
+            const user = await response.json();
+            return user;
+        } else {
+            // if response is not OK
+            const message = await response.text();
+            throw new Error(response.statusText + " " + message);
+        }
+    } catch (error) {
+        throw new Error(error.message, { cause: error })
+    }
+};
+
+async function doLogout() {
+    try {
+        const response = await fetch(APIURL + '/logout', {
+            method: 'POST',
+            credentials: 'include'
+        });
+        if (response.ok) {
+            return true ;
+        } else {
+            const message = await response.text();
+            throw new Error(response.statusText + " " + message);
+        }
+    } catch (error) {
+        throw new Error(error.message, { cause: error });
+    }
+}
+
+export { doLogin , getUserInfo, doLogout, listPlanes, addReservation, listReservations, deleteReservation, listReservedSeats, updateSeat, listSeats};
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,97 @@
-import { useState } from 'react';
-import { Button, Col, Container, Row } from 'react-bootstrap';
-import { getGuess } from './API';
-
+import { useContext, useState, useEffect} from 'react';
+import { BrowserRouter, Outlet, Route, Routes} from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { ListOfPlanes } from './components/Planes'
+import { AddReservationForm  } from './components/AddReservation'
+import { ListOfReservations } from './components/Reservations'
+import { listPlanes, doLogin, getUserInfo, doLogout  } from './API';
+import { PageNotFound } from './components/PageNotFound'
+import { Navigation } from './components/Navbar'
+import { LoginForm } from './components/Login';
+import UserContext from './UserContext'
 
 function App() {
 
-  const [guess, setGuess] = useState(100);
+  const [isLogged, setIsLogged] = useState(false);
+  const [user, setUser] = useState(null) ;
 
-  const newGuess = async () => {
+  const [planes, setPlanes] = useState([])
+
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const user = await getUserInfo();  
+        setUser(user);
+        setIsLogged(true);
+      } catch (err) {
+        setUser(null);
+        setIsLogged(false);
+      }
+    };
+    init();
+  }, []);  
+  
+  const handleLogin = async (email, password) => {
     try {
-      const n = await getGuess();
-      setGuess(n);
-    } catch (e) {
-      console.log(e);
+      const user = await doLogin(email, password) ;
+      setUser(user) ;
+      setIsLogged(true);
+    } catch (error) {
+      throw err;
     }
   }
 
-  return (
-    <Container>
-      <Row>
-        <h1>App</h1>
-      </Row>
-      <Row>
-        <Col><Button onClick={newGuess}>Guess a number</Button></Col>
-        <Col>{guess}</Col>
-      </Row>
-    </Container>
-  )
+
+  const handleLogout = async () => {
+    try {
+      await doLogout() ;
+      setIsLogged(false);
+      setUser(null);
+    } catch (error) {
+      throw err;
+    }
+    
+  }
+
+  useEffect(() => {
+    listPlanes().then((list) => {
+      setPlanes(list);
+    }).catch((err) => {
+      throw err;
+    })
+  }, [])
+
+
+  return( <UserContext.Provider value={user}>
+        <BrowserRouter>
+            <Navigation user={user} handleLogout={handleLogout} isLogged={isLogged}/> 
+            <Routes>
+              <Route element={<MainLayout handleLogout={handleLogout} user={user} isLogged={isLogged}/>}>
+                <Route index element={<ListOfPlanes planes={planes} />} />
+                <Route path='/planes/:planeId/:planeType/:seatsRow' element={<AddReservationForm  />} />
+                <Route path='/reservations' element={<ListOfReservations /> } />
+                <Route path='/login' element={<LoginForm handleLogin={handleLogin}/>}/>
+                <Route path='*' element={<PageNotFound />} />
+              </Route>
+            </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
+    )
 }
+
+
+function MainLayout(props) {
+  return <>
+    <main>
+      <Container>
+        <Outlet />
+      </Container>
+    </main>
+  </>
+}
+
 
 export default App
